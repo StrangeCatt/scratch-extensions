53663e00d1aac6f244d09450e8824904d5669627
StrangeCatt <84803484+StrangeCatt@users.noreply.github.com>
2022-03-16 16:08:22 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -14,12 +14,28 @@ class Fetch {
               defaultValue: 'https://extensions.turbowarp.org/hello.txt'
             }
           }
+        },
+        {
+          opcode: 'get2',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'GET2 [URL]',
+          arguments: {
+            URL: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'bonjour'
+            }
+          }
         }
       ]
     };
   }
 
   get (args) {
+    return fetch(args.URL)
+      .then(r => r.text())
+      .catch(() => '');
+  }
+   get2 (args) {
     return fetch(args.URL)
       .then(r => r.text())
       .catch(() => '');
