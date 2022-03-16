3728234f0ec23d8da8704875f5f76bb684f467b2
StrangeCatt <84803484+StrangeCatt@users.noreply.github.com>
2022-03-16 15:57:54 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,45 +1,29 @@
-(function (ext) {
-	var ExtName = 'test';
-	ext._shutdown = function() {};
-	ext._getStatus = function() {
-		return {status: 2, msg: 'Ready'};
-	};
-	var descriptor = {
-	        blocks: [
-	            [' ', 'svdfsbt', 'secId1'],
-	            [' ', 'svdfsbt', 'secId2'],
-	            [' ', 'svdfsbt', 'secId3'],
-	            [' ', 'svdfsbt', 'secId4'],
-	            [' ', 'svdfsbt', 'secId5', ''],
-	            [' ', 'svdfsbt', 'secId6', 'bg'],
-	        ],
-			menus: {
-			},
-	        url: 'b'
-	};
-	ext.secId1 = function(ext,callback){
-		
-		callback();
-	};
-	ext.secId2 = function(ext,callback){
-		
-		callback();
-	};
-	ext.secId3 = function(ext,callback){
-		
-		callback();
-	};
-	ext.secId4 = function(ext,callback){
-		
-		callback();
-	};
-	ext.secId5 = function(ext,callback){
-		
-		callback();
-	};
-	ext.secId6 = function(ext,callback){
-		
-		callback();
-	};
-	ScratchExtensions.register(ExtName, descriptor, ext);
-})({});
+class Fetch {
+  getInfo () {
+    return {
+      id: 'fetch',
+      name: 'Fetch',
+      blocks: [
+        {
+          opcode: 'get',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'GET [URL]',
+          arguments: {
+            URL: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'https://extensions.turbowarp.org/hello.txt'
+            }
+          }
+        }
+      ]
+    };
+  }
+
+  get (args) {
+    return fetch(args.URL)
+      .then(r => r.text())
+      .catch(() => '');
+  }
+}
+
+Scratch.extensions.register(new Fetch());
